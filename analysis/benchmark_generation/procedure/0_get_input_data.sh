ae71ba66efd3c5128ed711d826dd70a8c283ca77
zaneveld <zaneveld@gmail.com>
2025-11-07 13:44:34 -0800

    Added message about input data

---
+++ analysis/benchmark_generation/procedure/0_get_input_data.sh
@@ -0,0 +1,32 @@
+###############################################################################
+# Download data for InSilicoSeq 
+###############################################################################
+
+# Overview
+# This script will extract reads from K-mer previously defined clusters
+
+# Notes:
+
+# Always either use sbatch or grab a node using salloc before running the procedue - 
+# we never want to run anything on the login node.
+
+# This script is set up to be run as a BASH script (using source name_of_this_script.sh)
+# Therefore, all comments will be behind # signs. Actual code goes outside # lines
+# Each command (or group of commands) will be labelled with a comment line
+
+
+###############################################################################
+# Analysis
+###############################################################################
+
+# Report start of analysis
+echo "Starting"
+
+#NOTE: Dr. Brown has the input genomes
+#We might want to reverse engineer an NCBI Datasets command to download them 
+#automatically in the first step.
+
+
+
+#Report end of analysis
+echo "Done"
